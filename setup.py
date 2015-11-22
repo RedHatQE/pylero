c22a57c14ef22c3433d72b0bf7dc465828b3ca3a
or.granit <ogranit@redhat.com>
2015-11-22 18:24:12 +0200

    pylarion.cfg maintenace improvment: when we add new fields to 'pylarion.cfg' we dont want the user to add them manually, we also dont want to overwrite the user's old .cfg values. so I added a function to setup.py that recives two parameters: 1. our .cfg file-path 2. the existing .cfg file (if there is no such file we handle it internally) the function solve our problem so I we no longer need to add 'pylarion.cfg' to its dedicated libary using 'distutils' so i removed this from setup.py
    Change-Id: Idd99157440bec4ab9c81f4a298f26a1b28b10184

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
 import os
+import ConfigParser
 from distutils.core import setup
+
+old_file = '/etc/pylarion/pylarion.cfg'
+new_file = 'etc/pylarion/pylarion.cfg'
 
 
 def get_settings_dir():
     return os.path.join('/etc', 'pylarion')
 
+
+def update_config_file(path_to_old_file, path_to_new_file):
+    """if a .cfg file already exists, we add the new fields
+    from the prject .cfg file (if there are such fields).
+    if there isnt a .cfg file we create it at the given path.
+    """
+    section = 'webservice'
+    new_configs = []
+    old_configs = []
+    existing_keys = []
+
+    config = ConfigParser.ConfigParser()
+    config.read(path_to_new_file)
+
+    if(os.path.isfile(path_to_old_file)):
+        new_configs = config.items(section)
+        config.remove_section(section)
+        config.read(path_to_old_file)
+        old_configs = config.items(section)
+        existing_keys = map(lambda x: x[0], old_configs)
+        for element in new_configs:
+            if(element[0] not in existing_keys):
+                config.set(section, element[0], element[1])
+
+    with open(path_to_old_file, 'w') as configfile:
+        print("Writing pylarion.cfg to " + path_to_old_file)
+        config.write(configfile)
 
 if __name__ == "__main__":
     setup(
@@ -24,7 +55,9 @@ if __name__ == "__main__":
             'scripts/pylarion',
         ],
         data_files=[
-            (get_settings_dir(), ['etc/pylarion/pylarion.cfg',
-                                  'etc/pylarion/newca.crt']),
+            (get_settings_dir(), ['etc/pylarion/newca.crt'])
         ],
     )
+    update_config_file(
+        old_file,
+        new_file)
