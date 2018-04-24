fa64bdc360d997a9dbb1c8e32dd95959b37350b7
Xiaonan He <xhe@redhat.com>
2018-04-24 11:34:51 +0800

    improve pylarion-cmd
    Add the feature of creating document in polarion from external file or
    text. It's for solving the problem of importing document to polarion.
    The document with HTML markup can be all supported.
    
    One scenario:
    Importing a google (or LibreOffice) document to polarion via command line.
    Here are two steps:
    1) download Gdoc as .html file
    2) use pylaron-cmd to import it polarion
    
    For example:
    $ pylarion-cmd update --is_document --doc_with_space='KernelStorageQE/DOC-17'
      --doc_content='/tmp/gdoc.html'
      Created document 'KernelStorageQE/DOC-17'
       - document author      : xhe
       - document type        : generic
       - allowed workitem type: testcase
       - created date         : 2018-04-24 11:25:10.000761
       - document content is from: /root/my_gdoc1.html
    
    Change-Id: I5b7f9ff684dcf19c4be75092c7ed5c0a6ebab2f9
    Signed-off-by: Xiaonan He <xhe@redhat.com>

--- src/pylarion/cli/cmd.py
+++ src/pylarion/cli/cmd.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import datetime
+import os
 from pylarion.document import Document
 from pylarion.work_item import TestCase
 from pylarion.work_item import Requirement
@@ -23,17 +24,17 @@ class CmdList(object):
         return doc_list
 
     def print_documents(self, docs):
-        print 'Created%7sAuthor%7sDocument' % ('', '')
-        print '-------%7s------%7s--------' % ('', '')
+        print('Created%7sAuthor%7sDocument' % ('', ''))
+        print('-------%7s------%7s--------' % ('', ''))
 
         for doc in docs:
-            print '%-14s %-11s %s' % (doc.created.strftime('%Y-%m-%d'),
+            print('%-14s %-11s %s' % (doc.created.strftime('%Y-%m-%d'),
                                       doc.author,
-                                      doc.document_id)
+                                      doc.document_id))
 
     def list_workitems_in_doc(self, doc_name_with_space):
         if doc_name_with_space.find('/') < 0:
-            print "Document format should be: 'space/document'"
+            print("Document format should be: 'space/document'")
             exit(1)
 
         doc = Document(Document.default_project, doc_name_with_space)
@@ -53,14 +54,14 @@ class CmdList(object):
 
     def print_workitems(self, workitems):
         number = len(workitems)
-        print '\nTotal workitems: %d' % number
-        print 'Created%7sType%10sID%12sTitle' % ('', '', '')
-        print '---------%5s-------%7s-----%9s--------' % ('', '', '')
+        print('\nTotal workitems: %d' % number)
+        print('Created%7sType%10sID%12sTitle' % ('', '', ''))
+        print('---------%5s-------%7s-----%9s--------' % ('', '', ''))
 
         for wi in workitems:
             created = str(wi.created).split(' ')[0]
-            print '%-13s %-13s %-13s %s' % (created, wi.type,
-                                            wi.work_item_id, wi.title)
+            print('%-13s %-13s %-13s %s' % (created, wi.type,
+                                            wi.work_item_id, wi.title))
 
     def list_workitems_by_query(self, query, wi_type):
         fields = ['work_item_id',
@@ -88,29 +89,29 @@ class CmdList(object):
         for step in steps.steps:
             stp = step.values[0].content
             exp = step.values[1].content
-            print 'TestStep       = %s' % stp
-            print 'ExpectedResult = %s\n' % exp
+            print('TestStep       = %s' % stp)
+            print('ExpectedResult = %s\n' % exp)
 
         if steps.steps is None:
-            print 'No step for this tesecase!'
+            print('No step for this tesecase!')
 
     def print_links_for_requirement(self, req_id):
         req = Requirement(Requirement.default_project, req_id)
-        print 'ID%-12sRole' % ('')
-        print '-------%7s------' % ('')
+        print('ID%-12sRole' % (''))
+        print('-------%7s------' % (''))
 
         for linked in req.linked_work_items_derived:
-            print '%-13s %-13s' % (linked.work_item_id,
-                                   linked.role)
+            print('%-13s %-13s' % (linked.work_item_id,
+                                   linked.role))
 
     def print_links_for_testcase(self, case_id):
         tc = TestCase(TestCase.default_project, case_id)
-        print 'ID%-12sRole' % ('')
-        print '-------%7s------' % ('')
+        print('ID%-12sRole' % (''))
+        print('-------%7s------' % (''))
 
         for linked in tc.linked_work_items:
-            print '%-13s %-13s' % (linked.work_item_id,
-                                   linked.role)
+            print('%-13s %-13s' % (linked.work_item_id,
+                                   linked.role))
 
     def print_runs_by_query(self, query, is_template=False):
         query_ful = 'project.id:%s AND %s' % (TestRun.default_project, query)
@@ -137,31 +138,31 @@ class CmdList(object):
         preln = '------------%9s------%4s------' % ('', '')
         latln = '%2s--------%2s-------%9s--------' % ('', '', '')
 
-        print '%s %s' % (prestr, latstr)
-        print '%s %s' % (preln, latln)
+        print('%s %s' % (prestr, latstr))
+        print('%s %s' % (preln, latln))
 
         for tp in st:
             created_time = str(tp.created).split('.')[0]
-            print '%-20s %-9s %-8s %-10s%-15s %s' % (created_time,
+            print('%-20s %-9s %-8s %-10s%-15s %s' % (created_time,
                                                      tp.author,
                                                      tp.assignee,
                                                      tp.status,
                                                      tp.plannedin,
-                                                     tp.test_run_id)
+                                                     tp.test_run_id))
 
     def print_templates_by_query(self, query):
         self.print_runs_by_query(query, True)
 
     def print_testcases_from_run(self, run):
         tr = TestRun(run, None, TestRun.default_project)
-        print '(Only CaseID can be displayed when --run=$template)'
+        print('(Only CaseID can be displayed when --run=$template)')
         print('List cases for: %s\n' % run)
         ttstr = ('Created Time %8sStatus %1sExecutedBy %2sCaseID' % ('',
                                                                      '',
                                                                      ''))
         lnstr = ('------------%9s------%2s----------%3s------' % ('', '', ''))
-        print ttstr
-        print lnstr
+        print(ttstr)
+        print(lnstr)
 
         for rec in tr.records:
             time = str(rec.executed).split('.')[0]
@@ -180,11 +181,11 @@ class CmdList(object):
 
         ttstr = ('Due Date%-5sPlan ID%-24sPlan Name' % ('', ''))
         lnstr = ('-----------  ---------- %-20s---------' % '')
-        print ttstr
-        print lnstr
+        print(ttstr)
+        print(lnstr)
 
         for pl in pls:
-            print '%-12s %-30s %s' % (pl.due_date, pl.plan_id, pl.name)
+            print('%-12s %-30s %s' % (pl.due_date, pl.plan_id, pl.name))
 
 
 class CmdUpdate(object):
@@ -194,14 +195,14 @@ class CmdUpdate(object):
 
         run = run.strip()
         tr = TestRun(run, None, TestRun.default_project)
-        print '\nUpdate %s:' % run
+        print('\nUpdate %s:' % run)
 
         if not comment:
             comment = ''
 
-        print 'Total records: %d' % len(tr.records)
-        print 'Updated Date Time    Result  CaseID'
-        print '-------------------  ------  -----------'
+        print('Total records: %d' % len(tr.records))
+        print('Updated Date Time    Result  CaseID')
+        print('-------------------  ------  -----------')
 
         if user == 'None':
             user = TestRun.logged_in_user_id
@@ -213,12 +214,12 @@ class CmdUpdate(object):
             rec.result = result
             rec.comment = comment
 
-            print '%-20s %-7s %s' % (executed_str,
+            print('%-20s %-7s %s' % (executed_str,
                                      result,
-                                     rec.test_case_id)
+                                     rec.test_case_id))
             tr.update_test_record_by_object(rec.test_case_id,
                                             rec)
-        print 'Done!'
+        print('Done!')
 
     def update_all_case_results_for_runs(self, runs, result, user, comment):
         if runs.find(','):
@@ -226,7 +227,7 @@ class CmdUpdate(object):
                 self.update_all_case_results_for_run(run, result,
                                                      user, comment)
         else:
-            print "Please use comma ',' to seperate your runs!"
+            print("Please use comma ',' to seperate your runs!")
 
     def update_1_case_result_for_run(self,
                                      run,
@@ -239,7 +240,7 @@ class CmdUpdate(object):
             comment = ''
 
         tr = TestRun(run.strip(), None, TestRun.default_project)
-        print 'Update %s:' % run
+        print('Update %s:' % run)
 
         if user == 'None':
             user = TestRun.logged_in_user_id
@@ -254,14 +255,59 @@ class CmdUpdate(object):
                 rec.comment = comment
 
                 tr.update_test_record_by_object(testcase, rec)
-                print "%4sSet %s to %s (verdict comment: '%s')" % ('',
+                print("%4sSet %s to %s (verdict comment: '%s')" % ('',
                                                                    testcase,
                                                                    result,
-                                                                   comment)
+                                                                   comment))
                 return 0
 
         if not is_found:
-            print 'Test case %s is not found in run.' % testcase
+            print('Test case %s is not found in run.' % testcase)
+
+    def update_document(self,
+                        space,
+                        doc_name,
+                        doc_title,
+                        wi_types=None,
+                        doc_type=None,
+                        structure_link_role="parent",
+                        content=''):
+
+        cl = CmdList()
+        if cl.list_documents_by_query(doc_name):
+            print("Exit - Found same name '%s/%s'" % (space, doc_name))
+            return
+
+        file_path = ""
+        # the content could be file or data
+        if os.path.exists(content):
+            file_path = content
+            with open(content, mode='r') as check_file:
+                content = check_file.read()
+
+        doc = Document.create(Document.default_project,
+                              space,
+                              doc_name,
+                              doc_title,
+                              wi_types,
+                              doc_type,
+                              structure_link_role,
+                              content)
+
+        if cl.list_documents_by_query(doc_name):
+            print("Created document '%s/%s'" % (space, doc_name))
+            print(" - document author      : %s" % doc.author)
+            print(" - document type        : %s" % doc.type)
+            print(" - allowed workitem type: %s" % wi_types)
+            print(" - created date         : %s" % doc.created)
+            if file_path != "":
+                print(" - document content is from: %s" % file_path)
+            else:
+                print(" - document content     : %s" % content)
+            return True
+        else:
+            print("Failed to create document '%s/%s'" % (space, doc_name))
+            return False
 
     def update_run(self,
                    run,
@@ -292,7 +338,7 @@ class CmdUpdate(object):
 
         # Update run if exists, otherwise create it.
         if st:
-            print 'Update the existing run: %s' % run
+            print('Update the existing run: %s' % run)
             tr = TestRun(run,
                          None,
                          TestRun.default_project)
@@ -300,16 +346,16 @@ class CmdUpdate(object):
             # set fields
             if assignee != 'None':
                 tr.assignee = assignee
-                print '%4sSet Assignee to %s' % ('', assignee)
+                print('%4sSet Assignee to %s' % ('', assignee))
             if plannedin is not None:
                 tr.plannedin = plannedin
-                print '%4sSet Plannedin to %s' % ('', plannedin)
+                print('%4sSet Plannedin to %s' % ('', plannedin))
             if status is not None:
                 tr.status = status
-                print '%4sSet Status to %s' % ('', status)
+                print('%4sSet Status to %s' % ('', status))
             if description is not None:
                 tr.description = description
-                print '%4sSet Description to %s' % ('', description)
+                print('%4sSet Description to %s' % ('', description))
             tr.update()
 
         else:
@@ -322,14 +368,14 @@ class CmdUpdate(object):
                                 description=description)
             # display fields
             if assignee != 'None':
-                print '%4sSet Assignee to %s' % ('', assignee)
+                print('%4sSet Assignee to %s' % ('', assignee))
             if plannedin is not None:
-                print '%4sSet Plannedin to %s' % ('', plannedin)
+                print('%4sSet Plannedin to %s' % ('', plannedin))
             if status is not None:
-                print '%4sSet Status to %s' % ('', status)
+                print('%4sSet Status to %s' % ('', status))
             if description is not None:
-                print '%4sSet Description to %s' % ('', description)
-            print 'Created %s:' % run
+                print('%4sSet Description to %s' % ('', description))
+            print('Created %s:' % run)
 
     def update_runs(self,
                     runs,
@@ -347,6 +393,6 @@ class CmdUpdate(object):
                                 assignee,
                                 status,
                                 description)
-            print 'Done!'
+            print('Done!')
         else:
-            print "Please use comma ',' to seperate your runs!"
+            print("Please use comma ',' to seperate your runs!")
