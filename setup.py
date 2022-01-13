30b998ce1d5c9849f6b3ec13ce8cde39f3b79aaf
Wayne Sun <gsun@redhat.com>
2022-01-13 16:34:08 -0500

    Add more info in the package setup
    Add license, long description and classifiers info.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- setup.py
+++ setup.py
@@ -20,14 +20,21 @@ if sys.version_info >= (3, 0):
 else:
     install_requires_.insert(0, 'suds')
 
+# reading description from README.md
+with open("README.md", "r") as handle:
+    LONG_DESCRIPTION = handle.read()
+
 if __name__ == "__main__":
     setup(
         name=PACKAGE_NAME,
         version='0.0.1',
         description="Python SDK for Polarion",
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
         url="https://github.com/RedHatQE/pylero",
         author="%s Developers" % PACKAGE_NAME,
         author_email="szacks@redhat.com",
+        license='MIT',
         package_dir={
             PACKAGE_NAME: 'src/%s' % PACKAGE_NAME,
         },
@@ -42,5 +49,14 @@ if __name__ == "__main__":
         data_files=[
             ('', ['etc/%s/%s.cfg' % (PACKAGE_NAME, PACKAGE_NAME)])
         ],
-        install_requires=install_requires_
+        install_requires=install_requires_,
+        classifiers=[
+            'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'Intended Audience :: Developers',      # Define that your audience are developers
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',   # Again, pick a license
+            'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.8',
+          ]
     )
