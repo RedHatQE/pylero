ab07de2bd3dd527df615731b8fbeae5d1a161a59
Wayne Sun <gsun@redhat.com>
2022-01-27 09:34:56 -0500

    Update project development status
    The project have been stable and used in production for a while,
    update the status to reflect that.
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         ],
         install_requires=install_requires_,
         classifiers=[
-            'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
             'Intended Audience :: Developers',      # Define that your audience are developers
             'Topic :: Software Development :: Build Tools',
             'License :: OSI Approved :: MIT License',   # Again, pick a license
