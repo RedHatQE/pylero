d37dba6df8dce802937f981c4e1ae47cf38935f9
Wayne Sun <gsun@redhat.com>
2023-02-23 10:06:18 -0500

    Automatic commit of package [python-pylero] release [0.0.7-1].
    Created by command:
    
    /usr/bin/tito tag

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires_ = ["click", "suds"]
 if __name__ == "__main__":
     setup(
         name=PACKAGE_NAME,
-        version="0.0.6",
+        version="0.0.7",
         description="Python SDK for Polarion",
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
