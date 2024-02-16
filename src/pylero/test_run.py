3fb77dfda5cefecbb85090f61a4d7f2331781723
Leela Venkaiah G <lgangava@redhat.com>
2024-02-16 12:54:30 +0530

    conditionally import TestCase in TestRun module
    Signed-off-by: Leela Venkaiah G <lgangava@redhat.com>

--- src/pylero/test_run.py
+++ src/pylero/test_run.py
@@ -32,6 +32,11 @@ from pylero.text import Text
 from pylero.user import User
 from pylero.work_item import _WorkItem
 
+try:
+    from pylero.work_item import TestCase
+except ImportError:
+    pass
+
 # Build is used in custom fields.
 # Plan is used in custom fields.
 
@@ -930,7 +935,7 @@ class TestRun(BasePolarion):
             suds_object = test_record
         if test_record.result == "failed" and not test_record.defect_case_id:
             test_record.defect_case_id = create_incident_report(
-                self, test_record, _WorkItem(work_item_id=test_case_id)
+                self, test_record, TestCase(work_item_id=test_case_id)
             )
         self.session.test_management_client.service.addTestRecordToTestRun(
             self.uri, suds_object
@@ -1306,7 +1311,7 @@ class TestRun(BasePolarion):
         else:
             if test_record.result == "failed" and not test_record.defect_case_id:
                 test_record.defect_case_id = create_incident_report(
-                    self, test_record, _WorkItem(work_item_id=test_case_id)
+                    self, test_record, TestCase(work_item_id=test_case_id)
                 )
             index = test_case_ids.index(test_case_id)
             if isinstance(test_record, TestRecord):
